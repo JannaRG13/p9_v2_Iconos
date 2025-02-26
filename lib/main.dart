5e669d7afdf4346ce192702e88b48c7968ce46a2
JANNA_STEPHANIE_RAMIREZ_GRANADOS <a.22308051281287@cbtis128.edu.mx>
2025-02-26 20:43:48 +0000

    publicar captura

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,92 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Janna Ramirez G"),
+          titleTextStyle: TextStyle(color: Colors.black54, fontSize: 25),
+          centerTitle: true,
+          backgroundColor: Color(0xff9d6eed),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centra las filas verticalmente
+            children: <Widget>[
+              Text(
+                "     Janna Ramirez G \n Mat:22308051281287",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black54),
+              ), //
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  IconWithLabel(
+                      icon: Icons.star,
+                      label: 'Estrella',
+                      color: Colors.yellow),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                      icon: Icons.favorite,
+                      label: 'Corazón',
+                      color: Colors.red),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                      icon: Icons.thumb_up, label: 'Like', color: Colors.pink),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              // Segunda fila
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centra los iconos en la fila
+                children: <Widget>[
+                  IconWithLabel(
+                      icon: Icons.home, label: 'Casa', color: Colors.blue),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                      icon: Icons.settings,
+                      label: 'Ajustes',
+                      color: Colors.grey),
+                  SizedBox(width: 20), // Espacio entre los iconos
+                  IconWithLabel(
+                      icon: Icons.email, label: 'Email', color: Colors.red),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
+} // FIN DE CLASE MIS ICONOS
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class IconWithLabel extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color color;
+
+  IconWithLabel({required this.icon, required this.label, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Icon(icon, size: 50.0, color: color), // Icono con color personalizado
+        SizedBox(height: 8), // Espacio entre el icono y el texto
+        Text(label, style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
